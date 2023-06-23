5f9dbeb143a4b7d2280d9cd76a534c578c3957f4
Hawonee <5p3_10@naver.com>
2023-06-23 11:16:10 +0900

    first commit

---
+++ assets/js/gsap.js
@@ -0,0 +1,14 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".sticker-box img", {
+  
+});
+
+// const fadeinEls = document.querySelectorAll(".sticker-box img");
+
+// fadeinEls.forEach(function (textanimation, index) {
+//   gsap.to(textanimation, {
+//     delay: (index + 1) * 0.3,
+//     opacity: 1,
+//   });
+// });
